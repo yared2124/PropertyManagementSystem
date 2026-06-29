0b8b74745b8c7498ed369688f306107927931fda
yared aregayehu <yaredbaby21@gmail.com>
2026-06-29 06:02:22 -0500

    ReportController added

---
+++ backend/src/controllers/report.controller.js
@@ -0,0 +1,65 @@
+import reportService from "../services/report.service.js";
+import { successResponse } from "../utils/apiResponse.js";
+
+class ReportController {
+  async getRevenue(req, res, next) {
+    try {
+      const { startDate, endDate, groupBy } = req.query;
+      const data = await reportService.getRevenueReport(
+        startDate,
+        endDate,
+        groupBy,
+      );
+      res.status(200).json(successResponse(data));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getContracts(req, res, next) {
+    try {
+      const data = await reportService.getContractReport();
+      res.status(200).json(successResponse(data));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getMaintenance(req, res, next) {
+    try {
+      const data = await reportService.getMaintenanceReport();
+      res.status(200).json(successResponse(data));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getOccupancy(req, res, next) {
+    try {
+      const data = await reportService.getPropertyOccupancyReport();
+      res.status(200).json(successResponse(data));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      const data = await reportService.getUserReport();
+      res.status(200).json(successResponse(data));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getDashboardSummary(req, res, next) {
+    try {
+      const data = await reportService.getDashboardSummary();
+      res.status(200).json(successResponse(data));
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default new ReportController();
