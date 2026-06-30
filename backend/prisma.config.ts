3f05a6805ef911c65b95bae9c3c2e207606dc803
yared aregayehu <yaredbaby21@gmail.com>
2026-06-30 07:33:19 -0500

    updated

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL,
   },
 });
