b41c2e17fee686b86c6f7427b7e1bd4baff2c8ad
yared aregayehu <yaredbaby21@gmail.com>
2026-07-03 12:34:03 -0500

    vehuhcle.routes.js updated

--- backend/src/routes/vehicle.routes.js
+++ backend/src/routes/vehicle.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { authenticate } from "../middlewares/auth.js";
 import { requireRole } from "../middlewares/rbac.js";
 import { validate } from "../middlewares/validation.js";
+import { auditLog } from "../middlewares/auditLogger.js";
 import { vehicleSchema } from "../validations/vehicle.validation.js";
 import vehicleController from "../controllers/vehicle.controller.js";
 
@@ -9,19 +10,35 @@ const router = express.Router();
 
 router.use(authenticate);
 
+// --- Create vehicle ---
 router.post(
   "/",
   requireRole(["SYSTEM_ADMIN", "PROPERTY_MANAGER"]),
   validate(vehicleSchema),
+  auditLog("CREATE"),
   vehicleController.create,
 );
+
+// --- List vehicles ---
 router.get("/", vehicleController.findAll);
+
+// --- Get vehicle by ID ---
 router.get("/:id", vehicleController.findById);
+
+// --- Update vehicle ---
 router.put(
   "/:id",
   requireRole(["SYSTEM_ADMIN", "PROPERTY_MANAGER"]),
+  auditLog("UPDATE"),
   vehicleController.update,
 );
-router.delete("/:id", requireRole(["SYSTEM_ADMIN"]), vehicleController.delete);
+
+// --- Delete vehicle ---
+router.delete(
+  "/:id",
+  requireRole(["SYSTEM_ADMIN"]),
+  auditLog("DELETE"),
+  vehicleController.delete,
+);
 
 export default router;
