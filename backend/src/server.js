ba5609f5690443fad15d7d74237373578f3fdfca
yared aregayehu <yaredbaby21@gmail.com>
2026-07-04 16:56:48 -0500

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,11 @@
 
 import "dotenv/config";
 import app from "./app.js";
+import { registerListeners } from "./events/listeners.js";
+
 
 const PORT = process.env.PORT || 5000;
+registerListeners();
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
