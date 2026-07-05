ff493b190ca94d1f4be76a6b5d959a019a00062b
yared aregayehu <yaredbaby21@gmail.com>
2026-07-05 14:34:01 -0500

    updated

--- backend/src/events/eventBus.js
+++ backend/src/events/eventBus.js
@@ -1,7 +1,3 @@
-import { EventEmitter2 } from "eventemitter2";
-const eventBus = new EventEmitter2({
-  wildcard: true,
-  delimiter: ":",
-  maxListeners: 20,
-});
+import { EventEmitter } from "events";
+const eventBus = new EventEmitter();
 export default eventBus;
